e27007b5a6a93fcfa6f1c638390d2c4b8ea35677
Pyaephyo1561 <pyaephyo1561@gmail.com>
2025-02-09 15:24:27 +0630

    update

--- main.js
+++ main.js
@@ -2,60 +2,39 @@ const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 const menuBtnIcon = menuBtn.querySelector("i");
 
-menuBtn.addEventListener("click", (e) => {
+menuBtn.addEventListener("click",(e)=>{
   navLinks.classList.toggle("open");
+  
+  const isopen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class",isopen?"ri-close-line":"ri-menu-line");
+})
 
-  const isOpen = navLinks.classList.contains("open");
-  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
-});
-
-navLinks.addEventListener("click", (e) => {
+navLinks.addEventListener("click",(e)=>{
   navLinks.classList.remove("open");
-  menuBtnIcon.setAttribute("class", "ri-menu-line");
+  menuBtnIcon.setAttribute("class","ri-menu-line");
 });
 
 const scrollRevealOption = {
   distance: "50px",
-  origin: "bottom",
-  duration: 1000,
+  origin:"bottom",
+  duration:1000,
 };
-
-ScrollReveal().reveal(".header__container h2", {
+ScrollReveal().reveal(".header__container h2",{
   ...scrollRevealOption,
 });
-ScrollReveal().reveal(".header__container h1", {
+ScrollReveal().reveal(".header__container h1",{
   ...scrollRevealOption,
-  delay: 500,
+  delay:500,
 });
-ScrollReveal().reveal(".header__container p", {
+ScrollReveal().reveal(".header__container p",{
   ...scrollRevealOption,
-  delay: 1000,
+  delay:1000,
 });
-ScrollReveal().reveal(".header__btns", {
+ScrollReveal().reveal(".header__btns",{
   ...scrollRevealOption,
-  delay: 1500,
+  delay:1500,
 });
-
-ScrollReveal().reveal(".steps__card", {
+ScrollReveal().reveal(".step__card", {
   ...scrollRevealOption,
-  interval: 500,
-});
-
-ScrollReveal().reveal(".explore__card", {
-  duration: 1000,
-  interval: 500,
-});
-
-ScrollReveal().reveal(".job__card", {
-  ...scrollRevealOption,
-  interval: 500,
-});
-
-ScrollReveal().reveal(".offer__card", {
-  ...scrollRevealOption,
-  interval: 500,
-});
-
-const swiper = new Swiper(".swiper", {
-  loop: true,
+  interval:500,
 });
